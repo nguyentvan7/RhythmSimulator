91495e85d8a624e8c3fae80e68923986d26438fc
Van Nguyen <nguyentvan7@gmail.com>
2020-09-21 12:14:23 -0700

    Add option for no image operations

--- rhythm_folder.py
+++ rhythm_folder.py
@@ -17,6 +17,9 @@ def main():
     input_folder_name = ""
     output_folder_name = ""
     region_folder_name = ""
+    height = 0
+    width = 0
+    output = True
     
     arg_iter = iter(sys.argv)
     # Skip script name.
@@ -35,20 +38,27 @@ def main():
             elif argument == "--region" or argument == "-r":
                 argument = next(arg_iter)
                 region_folder_name = argument
+            elif argument == "--nooutput":
+                argument = next(arg_iter)
+                height = int(argument)
+                argument = next(arg_iter)
+                width = int(argument)
+                output = False
             else:
                 print("Invalid argument. Use -h or --help flag to print usage.")
                 exit(1)
-    if input_folder_name == "" or output_folder_name == "" or region_folder_name == "":
+    if (input_folder_name == "" and output) or output_folder_name == "" or region_folder_name == "":
         print("Invalid argument(s). Use -h or --help flag to print usage.")
         exit(1)
 
     # Check input folder.
-    if not path.exists(input_folder_name):
-        print("Input folder does not exist.")
-        exit(1)
-    if not path.isdir(input_folder_name):
-        print("Input is not a folder.")
-        exit(1)
+    if output:
+        if not path.exists(input_folder_name):
+            print("Input folder does not exist.")
+            exit(1)
+        if not path.isdir(input_folder_name):
+            print("Input is not a folder.")
+            exit(1)
     # Create output folder.
     if not path.exists(output_folder_name):
         os.mkdir(output_folder_name)
@@ -70,8 +80,11 @@ def main():
 
     regionlist = glob.glob(region_folder_name + "/*")
     regionlist = sorted(regionlist, reverse=True)
-    imagelist = glob.glob(input_folder_name + "/*")
-    imagelist = sorted(imagelist, reverse=True)
+    if output:
+        imagelist = glob.glob(input_folder_name + "/*")
+        imagelist = sorted(imagelist, reverse=True)
+    else:
+        imagelist = []
     imageregionlist = list(itertools.zip_longest(imagelist, regionlist))
     imageregionlist.reverse()
     # Used for rate.
@@ -95,9 +108,10 @@ def main():
         bitmask = []
         encoded_pixels = []
         row_offset = []
-        input_image = cv2.imread(image)
-        height, width, channels = input_image.shape
-        output_image = np.zeros((height, width, channels), np.uint8)
+        if output:
+            input_image = cv2.imread(image)
+            height, width, channels = input_image.shape
+            output_image = np.zeros((height, width, channels), np.uint8)
         # Check for full frame.
         if regions == None:
             input_regions = [[0.0, height, width, 0.0, 1 , 1]]
@@ -142,62 +156,67 @@ def main():
                                 pixelmask = 0b10
                         if pixelmask == 0b00:
                             pixels += 2
-                            encoded_pixels.append(input_image[row, col*2])
-                            encoded_pixels.append(input_image[row, col*2+1])
+                            if output:
+                                encoded_pixels.append(input_image[row, col*2])
+                                encoded_pixels.append(input_image[row, col*2+1])
                             break
                 rowmask.append(pixelmask)
             bitmask.append(rowmask)
         bitmasks.appendleft(bitmask)
         row_offsets.appendleft(row_offset)
-        print(datetime.now()-start)
         # Fill last row.
         while (pixels) % width != 0:
-            encoded_pixels.append(zero_p)
-            encoded_pixels.append(zero_p)
+            if output:
+                encoded_pixels.append(zero_p)
+                encoded_pixels.append(zero_p)
             pixels += 2
 
         # Convert list to image.
         pixel_touches += pixels
         bitmask_touches += pixels/2
         row_offset_touches += height
-        encoded_image = np.reshape(np.array(encoded_pixels, dtype=np.uint8), (-(-pixels//width), width, channels))
-        encoded_images.appendleft(encoded_image)
+        if output:
+            encoded_image = np.reshape(np.array(encoded_pixels, dtype=np.uint8), (-(-pixels//width), width, channels))
+            encoded_images.appendleft(encoded_image)
 
         # Decode image.
         for row in range(height):
+            # Assume we cache the row_offset for each of the 4 frames at each row.
+            row_offset_touches += 4
+            # Assume we cache the bitmask for the row for each of the 4 frames at each row.
+            bitmask_touches += 4*width//2
             for col in range(width//2):
                 pixelmask = bitmask[row][col]
-                bitmask_touches += 1
                 if pixelmask == 0b01:
                     # Skip, check previous frames, most recent first, but skipping current.
-                    for frame in range(1, len(encoded_images)):
+                    for frame in range(1, len(bitmasks)):
                         if bitmasks[frame][row][col] == 0b00 or bitmasks[frame][row][col] == 0b10:
-                            bitmask_touches += 1
-                            r, c, count = get_px_index(encoded_images[frame], bitmasks[frame][row], row_offsets[frame][row], col)
-                            output_image[row, col*2] = encoded_images[frame][r][c]
-                            output_image[row, col*2+1] = encoded_images[frame][r][c+1]
+                            if output:
+                                r, c = get_px_index(width, bitmasks[frame][row], row_offsets[frame][row], col)
+                                output_image[row, col*2] = encoded_images[frame][r][c]
+                                output_image[row, col*2+1] = encoded_images[frame][r][c+1]
                             pixel_touches += 2
-                            bitmask_touches += count
-                            row_offset_touches += 1
                             break
                 elif pixelmask == 0b00 or pixelmask == 0b10:
                     # Regional or strided pixel.
-                    r, c, count = get_px_index(encoded_image, bitmask[row], row_offset[row], col)
-                    output_image[row, col*2] = encoded_image[r][c]
-                    output_image[row, col*2+1] = encoded_image[r][c+1]
+                    if output:
+                        r, c = get_px_index(width, bitmask[row], row_offset[row], col)
+                        output_image[row, col*2] = encoded_image[r][c]
+                        output_image[row, col*2+1] = encoded_image[r][c+1]
                     pixel_touches += 2
-                    bitmask_touches += count
-                    row_offset_touches += 1
                 elif pixelmask == 0b11:
                     # Non-regional, make a black pixel.
-                    output_image[row, col*2] = 0
-                    output_image[row, col*2+1] = 0
+                    if output:
+                        output_image[row, col*2] = 0
+                        output_image[row, col*2+1] = 0
                             
                         
         # Save image
-        output_name = image.split('/')[1]
-        cv2.imwrite(output_folder_name + '/' + output_name, output_image)
-        cv2.imwrite(output_folder_name + '/encoded/' + output_name, encoded_image)
+        output_name = str(images)
+        if output:
+            output_name = image.split('/')[1]
+            cv2.imwrite(output_folder_name + '/' + output_name, output_image)
+            cv2.imwrite(output_folder_name + '/encoded/' + output_name, encoded_image)
         images += 1
         print(int(images/filecount*100), "% done (", images, "/", filecount, ")", sep="")
         # Stat calculation
@@ -220,19 +239,18 @@ def main():
     print("Completed in ", datetime.now()-start, "! Check ./", output_folder_name, " for decoded and encoded frames.", sep="")
     return
 
-def get_px_index(encoded_image, rowmask, row_offset, col):
+def get_px_index(width, rowmask, row_offset, col):
     count = 0
     for i in range(col):
         if rowmask[i] == 0b00:
             # Regional pixel.
             count += 2
 
-    encoded_height, width, channels = encoded_image.shape
     regional_px_cnt = row_offset + count
     row = regional_px_cnt // width
     col = regional_px_cnt % width
 
-    return row, col, count//2
+    return row, col
                 
 def print_usage():
     print("""\
